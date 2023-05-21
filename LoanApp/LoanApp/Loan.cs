0d96a57e4fb6a2536a930740e1b1676b3fb3a012
nhicaofromthecloud <114954151+nhicaofromthecloud@users.noreply.github.com>
2023-05-21 15:11:47 -0700

    Add files via upload

---
+++ LoanApp/LoanApp/Loan.cs
@@ -0,0 +1,52 @@
+﻿using LoanApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanApp
+{
+    internal class Loan
+    {
+        public string LoanHolder { get; set; }
+        public double InitialLoan { get; set; }
+        public double InterestRate { get; set; }
+
+        char loanType = 'U';
+        
+        public char LoanType
+        {   
+            get
+            { if (InterestRate == 3.7)
+                {
+                    loanType = 'H';
+                }
+             else if(InterestRate == 1.7)
+                {
+                    loanType = 'L';
+                }
+                return loanType;
+            }
+
+            set { }
+        }
+
+        public Loan (string loanHolder, double interestRate, double initialLoan) //pls remember to add datatype when declare a variable
+        {
+            LoanHolder = loanHolder;
+            InterestRate = interestRate;
+            InitialLoan = initialLoan;
+            
+        }
+
+        public override string ToString()
+        {
+            string outputStr = "\nLoan Holder: " + LoanHolder +
+                              "\nInterest Rate: "+ InterestRate +
+                              "\nLoanType: " + LoanType +
+                              "\nInitial Loan: " + InitialLoan.ToString("C");
+            return outputStr;
+        }
+    }
+}
---
+++ LoanApp/LoanApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LoanApp
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            GetDetails(out string loanHolder, out double interestRate, out double initialLoan);
+            Loan Loan1 = new Loan(loanHolder, interestRate, initialLoan);
+            WriteLine(Loan1);
+
+            WriteLine("Processing loan and debt payment data...");
+           
+            Write("Please enter yearly payment: ");
+            double.TryParse(ReadLine(), out double yearlyPayment);
+            int currentYear = 1;
+            double currentBalance = initialLoan;
+            while (currentBalance > -1)
+            {
+                WriteLine("Year "+ currentYear +"     Current Balance: "+ currentBalance+ "     Yearly Payment: "+ yearlyPayment);
+                currentYear++;
+                currentBalance -= yearlyPayment;
+               
+            }
+      
+            
+             
+        }
+
+        static void GetDetails(out string loanHolder, out double interestRate, out double initialLoan)
+        {
+            WriteLine("Please enter student name: ");
+            loanHolder= ReadLine();
+            while(loanHolder =="")
+            {
+                WriteLine("Student name cannot be empty. Please re-enter");
+                loanHolder = ReadLine();
+            }
+
+            //WriteLine("Please enter student ID: ");
+            //string studentId = ReadLine();
+
+            WriteLine("Please enter interest rate: ");
+            double.TryParse(ReadLine(), out interestRate);
+                       
+            WriteLine("Please enter initial loan: ");
+            double.TryParse(ReadLine(), out initialLoan);
+          
+
+        }
+    }
+}
